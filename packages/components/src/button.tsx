b9ebd00fe6044bfa04421d3786c8d39ef87f3006
黄晓刚 <627284482@qq.com>
2022-03-18 16:46:08 +0800

    Initialize examples

---
+++ packages/components/src/button.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export const Button = () => (
+  // eslint-disable-next-line no-alert
+  <button type="button" onClick={() => alert("you clicked me")}>
+    Click me
+  </button>
+);
